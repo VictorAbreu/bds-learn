e143fb8febe8cc755bad12807194b75e8c902893
Victor Abreu <vlabreu@outlook.com>
2021-11-10 08:43:26 -0300

    fix: notification

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -21,11 +21,11 @@ public class NotificationService {
 	private AuthService authService;
 	
 	@Transactional(readOnly = true)
-	public Page<NotificationDTO> notificationsFotCurrentUser(Pageable pageable){
+	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable){
 		
 		User user = authService.authenticated();
 		
-		Page<Notification> page = repository.findByUser(user, pageable);
+		Page<Notification> page = repository.find(user, unreadOnly, pageable);
 		
 		return page.map(x -> new NotificationDTO(x));
 	}
